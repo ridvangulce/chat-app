e53b02916ddef26dd1235d114501a73764939c7c
ridvangulce <ridvangulce@gmail.com>
2023-01-31 03:18:55 +0300

    +setup client

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+
+const { Server } = require("socket.io")
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "https://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    })
+});
+
+
+
+
+server.listen(3001, () => {
+    console.log("SERVER RUNNING");
+})
